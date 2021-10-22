424da4d0f6afd0d12bb51fd67e9eac5cb50eff3e
mytnitskaya <mariya.mytnitskaya@rubius.com>
2021-10-22 13:18:54 +0700

    add facades

--- DataAccess/Facades/CoachFacade.cs
+++ DataAccess/Facades/CoachFacade.cs
@@ -1,7 +1,36 @@
-﻿namespace DataAccess.Facades
+﻿using System;
+using System.Linq;
+using DataAccess.Models;
+
+namespace DataAccess.Facades
 {
     public class CoachFacade
     {
-        
+        private Coach _model;
+
+        public CoachFacade()
+        {
+            _model = new Coach();
+        }
+
+        public Coach GetModel()
+        {
+            return _model;
+        }
+
+        public Guid Id
+        {
+            get => _model.Id;
+        }
+
+        public string FullName
+        {
+            get => _model.LastName + " " + _model.FirstName + " " + _model.MiddleName;
+        }
+        public string FullNameShort
+        {
+            get => _model.LastName + " " + _model.FirstName.FirstOrDefault() + "." +
+                   _model.MiddleName.FirstOrDefault() + ".";
+        }
     }
 }
--- DataAccess/Facades/StudentFacade.cs
+++ DataAccess/Facades/StudentFacade.cs
@@ -1,7 +1,68 @@
-﻿namespace DataAccess.Facades
+﻿using System;
+using System.Collections.Generic;
+using DataAccess.Models;
+
+namespace DataAccess.Facades
 {
     public class StudentFacade
     {
-        
+        private Student _model;
+
+        public StudentFacade()
+        {
+            _model = new Student();
+        }
+        //разобраться зачем нам два конструктора и нужны ли оба
+        public StudentFacade(Student model)
+        {
+            _model = model;
+        }
+
+        public Student GetModel()
+        {
+            return _model;
+        }
+
+        public Guid Id
+        {
+            get => _model.Id;
+        }
+
+        public string FirstName
+        {
+            get => _model.FirstName;
+            set => _model.FirstName = value;
+        }
+
+        public string LastName
+        {
+            get => _model.LastName;
+            set => _model.LastName = value;
+        }
+
+        public string MiddleName
+        {
+            get => _model.MiddleName;
+            set => _model.MiddleName = value;
+        }
+
+        public DateTime BirthDate
+        {
+            get => _model.BirthDate;
+            set => _model.BirthDate = value;
+        }
+
+        public Rank Rank
+        {
+            get => (Rank)_model.Rank;
+            set => _model.Rank = (int) value;
+        }
+        //добавить switch для переделки в стринг если будут проблемы с вьюшкой
+
+        public List<Coach> Coaches
+        {
+            get => _model.Coaches;
+            set => _model.Coaches = value;
+        }
     }
 }
