3d375e32e6bf0daa00febeb550e2e5cf1196e344
mytnitskaya <mariya.mytnitskaya@rubius.com>
2021-10-26 18:29:20 +0700

    add context

---
+++ DataAccess/Data/AthleticsDocsContext.cs
@@ -0,0 +1,25 @@
+﻿using DataAccess.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Data
+{
+    public class AthleticsDocsContext : IdentityDbContext
+    {
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Coach> Coaches { get; set; }
+        public DbSet<Competition> Competitions { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<LevelOfTraining> LevelsOfTraining { get; set; }
+        public DbSet<Organization> Organizations { get; set; }
+        public DbSet<Person> People { get; set; }
+        public DbSet<Rank> Ranks { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        public AthleticsDocsContext(DbContextOptions<AthleticsDocsContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
