5b432b3af4a6c6e9f7634121c13567d33cd1332c
mytnitskaya <mashka7737@gmail.com>
2021-11-01 19:04:05 +0700

    add fields (City, Organization, Group) in Create Student view/controller

--- AthleticsDocs/Controllers/StudentsController.cs
+++ AthleticsDocs/Controllers/StudentsController.cs
@@ -30,6 +30,9 @@ namespace AthleticsDocs.Controllers
         public IActionResult Create()
         {
             RanksDropDownList();
+            CitiesDropDownList();
+            OrganizationsDropDownList();
+            GroupsDropDownList();
 
             return View();
         }
@@ -52,16 +55,35 @@ namespace AthleticsDocs.Controllers
                 //Log the error (uncomment dex variable name and add a line here to write a log.)
                 ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
             }
-            RanksDropDownList(student.Rank);
+            RanksDropDownList(student.RankId);
+            CitiesDropDownList(student.CityId);
+            OrganizationsDropDownList(student.OrganizationId);
+            GroupsDropDownList(student.GroupId);
             return View(student);
         }
 
         private void RanksDropDownList(object selectedRank = null)
         {
-
             //var ranksList = _context.Ranks.Select(x => x).OrderBy(x => x.Name);
             var ranksList = _context.Ranks.Select(x => x);
-            ViewBag.RanksList = new SelectList(ranksList, nameof(Rank.Id), nameof(Rank.Name));
+            ViewBag.RanksList = new SelectList(ranksList, nameof(Rank.Id), nameof(Rank.Name), selectedRank);
+        }
+
+        private void CitiesDropDownList(object selectedCity = null)
+        {
+            var citiesList = _context.Cities.Select(x => x).OrderBy(x => x.Name);
+            ViewBag.CitiesList = new SelectList(citiesList, nameof(City.Id), nameof(City.Name), selectedCity);
+        }
+        private void OrganizationsDropDownList(object selectedOrganization = null)
+        {
+            var organizationsList = _context.Organizations.Select(x => x).OrderBy(x => x.Name);
+            ViewBag.OrganizationsList = new SelectList(organizationsList, nameof(Organization.Id), 
+                nameof(Organization.Name), selectedOrganization);
+        }
+        private void GroupsDropDownList(object selectedGroup = null)
+        {
+            var groupsList = _context.Groups.Select(x => x).OrderBy(x => x.Name);
+            ViewBag.GroupsList = new SelectList(groupsList, nameof(Group.Id), nameof(Group.Name), selectedGroup);
         }
     }
 }
--- DataAccess/Models/Student.cs
+++ DataAccess/Models/Student.cs
@@ -10,8 +10,11 @@ namespace DataAccess.Models
         public Person Person { get; set; }
         public Guid? RankId { get; set; }
         public Rank Rank { get; set; }
+        public Guid? CityId { get; set; }
         public City City { get; set; }
+        public Guid? OrganizationId { get; set; }
         public Organization Organization { get; set; }
+        public Guid? GroupId { get; set; }
         public Group Group { get; set; }
     }
 }
